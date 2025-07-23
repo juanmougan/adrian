dc3b35f91b00ad327838e905d09b4f3797c5db40
Juan Manuel Mougan <juanmougan@gmail.com>
2025-07-23 21:17:35 +0200

    feat: horrible, yet functional UI alongside the API

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   resources :adrs, only: [ :create, :update, :index, :show ]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :adrs, only: [ :create, :update, :index, :show ]
+    end
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "adrs#index"
 end
